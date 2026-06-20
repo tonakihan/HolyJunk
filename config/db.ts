5f92dbae46a2dedbc286471a78467dcd8b2b3ccf
tonakihan <116086641+tonakihan@users.noreply.github.com>
2026-06-20 21:13:53 +0300

    fix DB_URL

--- config/db.ts
+++ config/db.ts
@@ -8,7 +8,7 @@ const DB_PASSWORD = await readSecretFile("db_pass.secret");
 const DB_NAME: string = getRequiredEnv("POSTGRES_DB");
 const DB_PORT: number = parseInt(process.env.DB_PORT ?? "5432");
 const DB_HOST: string = process.env.DB_HOST ?? "localhost";
-const DB_URL: string = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
+const DB_URL: string = `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`;
 
 if (DB_PORT < 1 || DB_PORT > 65535) {
   throw new Error(`Invalid DB_PORT: ${process.env.DB_PORT}`);
